cd17dbea1ccedaa1772ca3d3831e33c5f6dcfbc5
Abdullah Ali <103241883+abdlhli@users.noreply.github.com>
2022-09-23 00:05:26 +1200

    Add files via upload

---
+++ looping.dart
@@ -0,0 +1,14 @@
+void main() {
+  var i = 2;
+  print("LOOPING PERTAMA");
+  while (i <= 20) {
+    print("$i - I Love Coding ");
+    i += 2;
+  }
+  var j = 20;
+  print("LOOPING KEDUA");
+  while (j >= 2) {
+    print("$j - I Will Become A Mobile Developer ");
+    j -= 2;
+  }
+}
